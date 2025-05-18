ac5351bfc76f5fef3788c9b336e6d6a8797cfbf8
LeyCM <cm.ley.cm@gmail.com>
2025-05-18 18:08:25 +0200

    New Test Class for update for storage and the new section

--- src/test/java/org/leycm/test/Main.java
+++ src/test/java/org/leycm/test/Main.java
@@ -1,7 +1,7 @@
 package org.leycm.test;
 
 import org.leycm.storage.impl.JavaStorage;
-import org.leycm.storage.Storage;
+import org.leycm.storage.StorageBase;
 import org.leycm.storage.StorageRegistry;
 
 import java.util.logging.Logger;
@@ -13,10 +13,17 @@ public class Main {
 
         StorageRegistry.setup(null, logger);
 
-        Storage storage = Storage.of("test/path/file", Storage.Type.JSON, JavaStorage.class);
+        StorageBase storage = StorageBase.of("test/path/file", StorageBase.Type.JSON, JavaStorage.class);
 
-        String s = storage.get("test.name", String.class);
-        System.out.println(s);
+        storage.set("test.name.hans", "Hans");
+        storage.set("test.name.paul", "Paul");
+        storage.set("test.id.paul.", "paul-is-dumm");
+        storage.set("noch.ein.test", "Hans 2.0 Update");
+        storage.save();
 
+        System.out.println("key=\"\" deep=false : " + storage.getKeys(false));
+        System.out.println("key=\"\" deep=true : " + storage.getKeys(true));
+        System.out.println("key=\"test\" deep=false : " + storage.getKeys("test", false));
+        System.out.println("key=\"test\" deep=true : " + storage.getKeys("test", true));
     }
 }
