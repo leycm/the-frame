847e2b4b9b695b7106497a8f3d8c3bf85c4394bf
LeyCM <cm.ley.cm@gmail.com>
2025-05-19 17:21:30 +0200

    Tests for the Table classes

--- src/test/java/org/leycm/test/Main.java
+++ src/test/java/org/leycm/test/Main.java
@@ -10,20 +10,6 @@ public class Main {
     public static void main(String[] args) {
 
         Logger logger = Logger.getLogger("main");
-
-        StorageRegistry.setup(null, logger);
-
-        StorageBase storage = StorageBase.of("test/path/file", StorageBase.Type.JSON, JavaStorage.class);
-
-        storage.set("test.name.hans", "Hans");
-        storage.set("test.name.paul", "Paul");
-        storage.set("test.id.paul.", "paul-is-dumm");
-        storage.set("noch.ein.test", "Hans 2.0 Update");
-        storage.save();
-
-        System.out.println("key=\"\" deep=false : " + storage.getKeys(false));
-        System.out.println("key=\"\" deep=true : " + storage.getKeys(true));
-        System.out.println("key=\"test\" deep=false : " + storage.getKeys("test", false));
-        System.out.println("key=\"test\" deep=true : " + storage.getKeys("test", true));
+        
     }
 }
